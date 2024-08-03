4e98b95794904027f5babd5122fd7ce4303b3b02
“Brian <briananggriawan31@gmail.com>
2024-08-03 20:50:33 +0700

    Initial Commit

---
+++ My profile/My_profileApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_profileApp.swift
+//  My profile
+//
+//  Created by Brian Anggriawan on 03/08/24.
+//
+
+import SwiftUI
+
+@main
+struct My_profileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
