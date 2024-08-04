c6388f46312c6815a401b0561d3f9aa45a5545a9
“Brian <briananggriawan31@gmail.com>
2024-08-04 18:41:12 +0700

    first commit

--- My profile/ContentView.swift
+++ My profile/ContentView.swift
@@ -9,16 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(.systemBlue)
+                .ignoresSafeArea(.all)
+            VStack {
+                Image("profile")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 4)
+                    )
+                    .shadow(radius: 10)
+                Text("Brian Anggriawan")
+                    .foregroundColor(.white)
+                    .font(Font.custom("Pacifico-Regular", size: 30))
+                    .bold()
+                Text("Tech Manager | Backend Developer | Mobile Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 14))
+                Divider()
+                PrivateInfoView(title: "082228113269", iconName: "phone.fill")
+                PrivateInfoView(title: "briananggriawan31@gmail.com", iconName: "envelope.fill")
+                    
+            }
         }
-        .padding()
     }
 }
 
+
 #Preview {
     ContentView()
+}
+
+struct PrivateInfoView: View {
+    let title: String
+    let iconName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .frame(height: 50)
+            .foregroundColor(.white)
+            .overlay(
+                HStack {
+                    Image(systemName: iconName)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    Text(title)
+                        .foregroundColor(Color(.systemBlue))
+                }
+                
+            )
+            .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 10))
+    }
 }
